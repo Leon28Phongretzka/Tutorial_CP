0d3803c2cd2a95f9596db8bcf441c86a5cec64b2
Leon28Phongretzka <lamperougelelouchanm@gmail.com>
2022-10-10 20:16:01 +0700

    Tuto 1

---
+++ Tutorial1_3.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FAST_IO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0); cerr.tie(0)
+typedef long long ll;
+typedef unsigned long long ul;
+typedef long double ld;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<bool, ll> pbl;
+typedef pair<ul, int> pui;
+typedef vector<pii> vpii;
+typedef vector<vpii> vvpii;
+typedef vector<int> vi;
+typedef vector<int>::iterator ip;
+typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+typedef vector<pll> vpll;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef vector<pbl> vpbl;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<pdd> vpdd;
+typedef vector<string> vs;
+typedef vector<pui> vpui;
+typedef vector<ul> vu;
+typedef vector<string> vs;
+#define mp make_pair
+#define gl getline
+#define pb push_back
+#define eb emplace_back
+#define ob pop_back
+#define sz(s) ((int)(s.size()))
+#define UM uno\nrdered_map
+#define US uno\nrdered_set
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define fora(i, n) for(auto i:n)
+#define Len 1000005
+const double pi=3.14159265358979323846;
+const int MOD = 1000000007;
+ll tt;
+void solve() 
+{
+    char c; cin >> c;
+    cout << char(toupper(c)) << " " << char(tolower(c)) << endl;
+}
+int main() 
+{
+    FAST_IO;
+    // freopen("time.in", "r", stdin); freopen("time.out", "w", stdout);
+    cin >> tt; while(tt--) {solve();}
+    // solve();
+}
